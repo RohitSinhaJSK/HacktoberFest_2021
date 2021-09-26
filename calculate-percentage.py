5bf30d40dba658e514659c5844a40db0e83e6b2a
RohitSinhaJSK <85009869+RohitSinhaJSK@users.noreply.github.com>
2021-09-26 10:41:43 +0530

    Update calculate-percentage.py

--- calculate-percentage.py
+++ calculate-percentage.py
@@ -1,4 +1,4 @@
-print("Enter your marks: ")
+prin("Enter your marks: ")
 english1 = int(input("english paper 1: "))
 english2 = int(input("english paper 2: "))
 sindhi = int(input("sindhi: "))
@@ -11,8 +11,10 @@ phy = int(input("physics: "))
 maths = int(input("mathematics: "))
 
 a = english1 + english2 + sindhi + urdu + pakstd + islamiat + chem + bio + phy + maths
-print("obtained marks:", a)
+prin("obtained marks:", a)
 totalmarks = 850
-print("total marks:", totalmarks)
+prin("total marks:", totalmarks)
 percentage = (a / totalmarks) * 100
-print("percentage:", percentage)
+prin("percentage:", percentage)
+
+#i have removed "t" from 4 "print"
