66056f3ee24f7f0a518e16adfb3197895891de1e
RohitSinhaJSK <85009869+RohitSinhaJSK@users.noreply.github.com>
2021-09-26 10:51:01 +0530

    Update volume-of-sphere.py

--- volume-of-sphere.py
+++ volume-of-sphere.py
@@ -1,7 +1,7 @@
 """Write a python program to get the volume of a sphere, take the radius as input from user. V = 4/3 πr3""" 
 
-import math 
+import ma #not ma it's math
 radius = float(input("ENTER RADIUS: "))
 pi = math.pi
 volume = 4/3*(pi)*(radius**3)
-print("THE VOLUME OF SPHERE IS",volume)
+prin("THE VOLUME OF SPHERE IS",volume)
