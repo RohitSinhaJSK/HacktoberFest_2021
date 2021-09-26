3e2e972efaf284f605fa9a1b25c707ad4cadcc55
RohitSinhaJSK <85009869+RohitSinhaJSK@users.noreply.github.com>
2021-09-26 10:40:15 +0530

    Update age-classifier-program.py

--- age-classifier-program.py
+++ age-classifier-program.py
@@ -1,11 +1,11 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
-if (age <= 1):
+i (age <= 1):
   print("You are an infant")
-elif (age >1)and(age <13):
+eli (age >1)and(age <13):
   print("You are a kid")
-elif (age >13)and(age <20):
+eli (age >13)and(age <20):
   print("You are a teenager") 
 else :     
   print("You are an adult")
