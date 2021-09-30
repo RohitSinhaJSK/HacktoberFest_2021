6d03a45ebb4eb53569d961737568be60d0753522
qwpou <72141557+qwpou@users.noreply.github.com>
2021-09-30 22:48:36 +0530

    Update age-classifier-program.py

--- age-classifier-program.py
+++ age-classifier-program.py
@@ -1,9 +1,9 @@
 name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 
-i (age <= 1):
+if (age <= 1):
   print("You are an infant")
-eli (age >1)and(age <13):
+elif (age >1)and(age <13):
   print("You are a kid")
 eli (age >13)and(age <20):
   print("You are a teenager") 
