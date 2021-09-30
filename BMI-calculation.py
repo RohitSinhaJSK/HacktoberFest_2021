28c496180ed4fa65dfb412a8200d0882d29f65a9
tmforq <72141227+tmforq@users.noreply.github.com>
2021-09-30 22:42:16 +0530

    Update BMI-calculation.py

--- BMI-calculation.py
+++ BMI-calculation.py
@@ -5,18 +5,18 @@ weight = int(input("ENTER YOUR WEIGHT: "))
 hgt_unit = input("YOU WILL ENTER YOUR HEIGHT IN 'm' OR 'in'? ")
 hgt_unit = hgt_unit.lower()
 height = int(input("ENTER YOUR HEIGHT: "))
-i wgt_unit == "kg" and hgt_unit == "metres":
+if wgt_unit == "kg" and hgt_unit == "metres":
     BMI = weight/(height**2)
     print(f'YOUR BMI IS: {BMI}')
-eli wgt_unit == "lbs" and hgt_unit == "in":
+elif wgt_unit == "lbs" and hgt_unit == "in":
     BMI = (703 * weight) / (height**2)
     print(f'YOUR BMI IS: {BMI}')
 
-i BMI < 18.5:
+if BMI < 18.5:
     print("YOU ARE UNDERWEIGHT")
-eli BMI >= 18.5 and BMI < 24.9:
+elif BMI >= 18.5 and BMI < 24.9:
     print("YOU ARE NORMAL")
-eli BMI >= 25 and BMI < 29.9:
+elif BMI >= 25 and BMI < 29.9:
     print("YOU ARE OVERWEIGHT")
 else:
     print("YOU ARE OBESE")
